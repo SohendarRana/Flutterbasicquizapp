1397202731667e634c8e4d258d98d5d5b9fe8f88
SohendarRana <sohendar1@gmail.com>
2022-04-29 15:32:22 +0530

    day 1 completed

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,50 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerselect() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+
+    print(questionIndex);
+  }
+
+  @override
   Widget build(BuildContext context) {
+    var questions = [
+      "what's your name",
+      "what's your favourite animal",
+    ];
     return MaterialApp(
-      home: Text('Hello my app'),
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("Quiz App"),
+          ),
+          body: Column(
+            children: [
+              Text(questions[questionIndex]),
+              ElevatedButton(
+                child: Text("Answer 1"),
+                onPressed: answerselect,
+              ),
+              ElevatedButton(
+                child: Text("Answer 2"),
+                onPressed: answerselect,
+              ),
+              ElevatedButton(
+                child: Text("Answer 3"),
+                onPressed: answerselect,
+              ),
+            ],
+          )),
     );
   }
 }
